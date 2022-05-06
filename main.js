669f6be9150d2ae1950be6085dd980cccb71fb79
Wesley Sampaio <weslei.deus@gmail.com>
2022-05-06 12:15:10 -0300

    Stage 4

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
@@ -6,7 +14,14 @@ function onScroll() {
   }
 }
 
- 
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
@@ -14,3 +29,18 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
